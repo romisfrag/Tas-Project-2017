297b4da655571274faa5f955f37b584877a8f3e6
romisfrag <romisfrag@gmail.com>
2017-10-25 15:56:20 +0200

    it work but the latex js doesn't start need to fix it

--- script.ml
+++ script.ml
@@ -6,21 +6,29 @@ open Sexplib
 open TreePrinter
 
 
-let parse_check_return (s : string) : string  =
+(* let parse_check_return jstring = *)
+(*   let s = Js.to_string jstring in *)
+(*   let t = parse_term (Sexp.of_string s) in *)
+(*   (match type_check_with_tree t [] with *)
+(*   |Some (t,s,p) -> Js.string (print_proof_tree p) *)
+(*   |None -> (Js.string "Error")) *)
+
+
+
+let affiche_interne s2 =
+  let s = Js.to_string s2 in
   let t = parse_term (Sexp.of_string s) in
-  match type_check_with_tree t [] with
-  |Some (t,s,p) -> print_proof_tree p
-  |None -> "Error"
-
-
-
-
+  let res = (match type_check_with_tree t [] with
+  |Some (t,s,p) -> Js.string (print_proof_tree p)
+  |None -> (Js.string "Error")) in
+  res
+  
 
 (* We now export the function *)
 let _ =
   Js.export "obj_typeur"
             (object%js
-               method start_type s = parse_check_return s
+               method affiche x = affiche_interne x
              end)
 
 

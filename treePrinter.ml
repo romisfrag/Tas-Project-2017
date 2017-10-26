e06e83aec6788264c03876c4a7e9441e34446345
romisfrag <romisfrag@gmail.com>
2017-10-26 19:27:19 +0200

    tree_printer supposed to be good but not tested

--- treePrinter.ml
+++ treePrinter.ml
@@ -4,8 +4,9 @@ open Types
 let rec term_to_latex_rec (t : term) (contexte : named_contexte) : string =
   match t with
   | Var n -> let (name,ty) = List.nth contexte n in name
-  | Abs (name,st) -> "\\lambda " ^ name ^ "." ^ term_to_latex_rec st ((name,TVar "bidon") :: contexte)
+  | Abs (name,st) -> "\\lambda " ^ name ^ "." ^ term_to_latex_rec st ((name,Typ (TVar "bidon")) :: contexte)
   | Appl (st1,st2) -> "(" ^ term_to_latex_rec st1 contexte ^ " \\ " ^ term_to_latex_rec st2 contexte ^ ")"
+  | Let(name,st1,st2) -> failwith "lol"
 
 let term_to_latex (t : term) : string =
   term_to_latex_rec t []
@@ -16,15 +17,21 @@ let rec typ_to_latex (ty : typ) : string =
   | TVar s -> s
   | Arrow (ty1,ty2) -> "(" ^ typ_to_latex ty1 ^ "\\rightarrow " ^ typ_to_latex ty2 ^ ")"
 
+let rec sigma_to_latex (s : sigma) : string =
+  match s with
+  | Typ t -> typ_to_latex t
+  | Forall(name,ss) -> "\\forall " ^ name ^ ":" ^ sigma_to_latex ss
+               
+                                                                                       
 
 let rec env_to_latex (ctxt : named_contexte) : string =
   match ctxt with
   | [] -> "\\emptyset"
-  | (name,ty) :: next -> name ^ ":" ^ typ_to_latex ty ^ ";" ^ env_to_latex next
+  | (name,ty) :: next -> name ^ ":" ^ sigma_to_latex ty ^ ";" ^ env_to_latex next
 
                                                                               
 let print_goal (g : goal) : string =
-  env_to_latex g.ctxt ^ "\\vdash " ^ term_to_latex_rec g.ter g.ctxt^ ":" ^ typ_to_latex g.ty
+  env_to_latex g.ctxt ^ "\\vdash " ^ term_to_latex_rec g.ter g.ctxt^ ":" ^ sigma_to_latex g.ty
 
                                                                              
 let rec print_proof_tree (p : proofTree) : string =
